76065512aa441935a2c7bd928808441084929ccf
Mangofish369 <340868470@gapps.yrdsb.ca>
2023-09-29 12:04:38 -0400

    Added new vehicle subclass (Truck)
    - moves as slow as ambulance
    - can knock down pedestrians

---
+++ VehicleSim2023v021/Truck.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class truck here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Truck extends Vehicle
+{
+    /**
+     * Act - do whatever the truck wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Truck (VehicleSpawner origin){
+        super(origin);
+        
+        maxSpeed = 2.5;
+        speed = maxSpeed;
+        
+        GreenfootImage truck = getImage();
+        truck.scale(100,50);
+    }
+    public void act()
+    {
+        super.act();
+    }
+    public boolean checkHitPedestrian(){
+        Pedestrian p = (Pedestrian)getOneObjectAtOffset((int)speed + getImage().getWidth()/2, 0, Pedestrian.class);
+        if (p != null)
+        {
+            p.knockDown();
+            return true;
+        }
+        return false;
+    }
+}
